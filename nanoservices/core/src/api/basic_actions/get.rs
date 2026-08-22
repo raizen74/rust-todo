73d7ec89b991c48b5ab2dbda5a082ecd6cf87c8e
David Galera <raizen74@gmail.com>
2026-08-22 12:34:42 +0200

    get/{name} route

--- nanoservices/core/src/api/basic_actions/get.rs
+++ nanoservices/core/src/api/basic_actions/get.rs
@@ -1,7 +1,16 @@
 use crate::structs::{AllToDoItems, ToDoItem};
+use glue::errors::{NanoServiceError, NanoServiceErrorStatus};
 use to_do_dal::json_file::get_all as get_all_handle;
-use glue::errors::NanoServiceError;
 
 pub async fn get_all() -> Result<AllToDoItems, NanoServiceError> {
     Ok(AllToDoItems::from_hashmap(get_all_handle::<ToDoItem>()?))
+}
+
+pub async fn get_by_name(name: &str) -> Result<ToDoItem, NanoServiceError> {
+    Ok(get_all_handle::<ToDoItem>()?
+        .remove(name)
+        .ok_or(NanoServiceError::new(
+            format!("Item with name {} not found", name),
+            NanoServiceErrorStatus::NotFound,
+        ))?)
 }
--- nanoservices/networking/actix_server/src/api/basic_actions/mod.rs
+++ nanoservices/networking/actix_server/src/api/basic_actions/mod.rs
@@ -5,5 +5,10 @@ pub mod update;
 use actix_web::web::{ServiceConfig, get, scope};
 
 pub fn basic_actions_factory(app: &mut ServiceConfig) {
-    app.service(scope("/api/v1").route("get/all", get().to(get::get_all)));
+    app.service(
+        scope("/api/v1")
+            .route("get/all", get().to(get::get_all))
+            .route("get/{name}", get().to(get::get_by_name)),
+    );
+    // app.service(scope("/api/v1").route("get/{name}", get().to(get::get_by_name)));
 }
