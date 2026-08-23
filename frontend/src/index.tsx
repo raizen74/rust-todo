f7d32e8fdc313b5f1a437e78e83910740edd7c5e
David Galera <raizen74@gmail.com>
2026-08-23 12:39:54 +0200

    Embedded React makes API calls to Rust server localhost:8001/api/v1/get/all

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import getAll from "./api/get";
+import { ToDoItems } from "./interfaces/toDoItems";
 
 const App = () => {
+  const [data, setData] = useState<string | ToDoItems | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await getAll();
+      if (response.error) {
+        setError(response.error);
+      } else {
+        setData(response.data);
+      }
+    };
+    fetchData();
+  }, []); // fires once the App component has been loaded
   return (
     <div>
-      <h1>Hello World</h1>
+      {error ? (
+        <div style={{ color: "red" }}>Error: {error}</div>
+      ) : data ? (
+        <div>Data loaded: {JSON.stringify(data)}</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
