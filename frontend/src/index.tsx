572a972505bfddb609e3e0aa766b9f4bdc41ef0b
David Galera <raizen74@gmail.com>
2026-08-23 20:27:52 +0200

    toDoItem tsx component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import getAll from "./api/get";
 import { ToDoItems } from "./interfaces/toDoItems";
 import { CreateToDoItem } from "./components/createItemForm";
+import { ToDoItem } from "./components/toDoItem";
 
 const App = () => {
   const [data, setData] = useState<string | ToDoItems | null>(null);
@@ -18,8 +19,9 @@ const App = () => {
     };
     fetchData();
   }, []); // fires once the App component has been loaded
-  
+
   function reRenderItems(response: any) {
+    console.log("reRenderItems called with response:", response);
     if (response.error) {
       alert(JSON.stringify(response));
       return;
@@ -30,19 +32,52 @@ const App = () => {
       setError("Unknown error");
     }
   }
-  
-  return (
-    <div>
-      {error ? (
-        <div style={{ color: "red" }}>Error: {error}</div>
-      ) : data ? (
-        <div>Data loaded: {JSON.stringify(data)}</div>
-      ) : (
-        <div>Loading...</div>
-      )}
-      <CreateToDoItem passBackResponse={reRenderItems} />
-    </div>
-  );
+
+  if (error) {
+    return <div style={{ color: "red" }}>Error: {error}</div>;
+  } else if (!data || typeof data === "string") {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <div className="mainContainer">
+          <div className="header">
+            <p>complete tasks: {data.done.length}</p>
+            <p>pending tasks: {data.pending.length}</p>
+          </div>
+          <h1>Pending Items</h1>
+          <div>
+            {data.pending.map((item, index) => (
+              <>
+                <ToDoItem
+                  key={item.title + item.status}
+                  title={item.title}
+                  status={item.status}
+                  id={item.title}
+                  passBackResponse={reRenderItems}
+                />
+              </>
+            ))}
+          </div>
+          <h1>Done Items</h1>
+          <div>
+            {data.done.map((item, index) => (
+              <>
+                <ToDoItem
+                  key={item.title + item.status}
+                  title={item.title}
+                  status={item.status}
+                  id={item.title}
+                  passBackResponse={reRenderItems}
+                />
+              </>
+            ))}
+          </div>
+          <CreateToDoItem passBackResponse={reRenderItems} />
+        </div>
+      </div>
+    );
+  }
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
