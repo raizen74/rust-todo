c402209a2575c89bfbbd87f675b714747b7444a7
David Galera <raizen74@gmail.com>
2026-08-27 10:08:43 +0200

    Added cors to to-do-actix-server so it can be called from localhost:3000 (React)

--- nanoservices/to-do/networking/actix_server/src/main.rs
+++ nanoservices/to-do/networking/actix_server/src/main.rs
@@ -1,13 +1,22 @@
 mod api;
 use actix_web::{App, HttpServer};
+use actix_cors::Cors;
 use to_do_dal::migrations::run_migrations;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     run_migrations().await;
-    HttpServer::new(|| App::new().configure(api::views_factory))
-        .workers(4)
-        .bind("127.0.0.1:8001")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+        App::new()
+            .configure(api::views_factory)
+            .wrap(cors)
+    })
+    .workers(4)
+    .bind("127.0.0.1:8001")?
+    .run()
+    .await
 }
